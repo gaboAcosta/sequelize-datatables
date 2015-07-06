afb96c84140a437496957bb13d6b6d940e801b95
Gabo Acosta <gabo.acosta@cylonsoft.com>
2015-07-06 15:50:48 -0500

    Added search and pagination

--- test/builder.spec.js
+++ test/builder.spec.js
@@ -24,8 +24,55 @@ describe('DtBuilder Ordering', function() {
     it('should return a params object with Sequelize order options', function () {
       var expected, req, builder;
       req = require('./mocks/requests/base.json');
-      expected = {order:[['id','ASC']]};
+      expected = {order:[['id','ASC']],offset: "11",limit: "10"};
       builder = new Builder({},req);
+      expect(expected).to.deep.equal(builder.getParams());
+    });
+  });
+});
+describe('DtBuilder Search', function() {
+  context('The builder gets a search field in the request', function () {
+    it('should return a params object with Sequelize where options', function () {
+      var expected, req, builder;
+      req = require('./mocks/requests/search.json');
+      expected = {
+        order:[['id','ASC']],
+        where:{
+          $or: [
+            {id: { $like: '%test%'}},
+            {name: { $like: '%test%'}},
+            {email: { $like: '%test%'}},
+          ]
+        },
+        offset: "11",
+        limit: "10"
+      };
+      builder = new Builder({},req);
+      expect(expected).to.deep.equal(builder.getParams());
+    });
+  });
+});
+describe('DtBuilder Pagination', function() {
+  context('The builder gets a start and length parameter in the request', function () {
+    it('should return a params object with Sequelize offset and limit options', function () {
+      var expected, req, builder;
+      req = require('./mocks/requests/search.json');
+      expected = {
+        order:[['id','ASC']],
+        where:{
+          $or: [
+            {id: { $like: '%test%'}},
+            {name: { $like: '%test%'}},
+            {email: { $like: '%test%'}},
+          ]
+        },
+        offset: "11",
+        limit: "10"
+      };
+      builder = new Builder({},req);
+      console.log('===EXPECTED===');
+      console.log(expected);
+      console.log('===EXPECTED===');
       expect(builder.getParams()).to.deep.equal(expected);
     });
   });
